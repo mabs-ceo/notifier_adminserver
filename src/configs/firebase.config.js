817d95288e21ef2293c3fb9885109e1a2a2035f1
mabs-ceo <codepreneurceo@gmail.com>
2025-04-22 20:51:06 +0800

    service account key

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -1,7 +1,7 @@
 // firebase.js
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../etc/secrets/service-account-key.json");
+const serviceAccount = require("../etc/secrets/service-account-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
